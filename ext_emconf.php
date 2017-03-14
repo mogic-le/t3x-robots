55b605719abad7bf429d9b8fcf7b59fcaee71bc0
Christian Weiske <weiske@mogic.com>
2017-03-14 15:09:46 +0100

    Release 1.0.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = array (
   'conflicts' => '',
   'priority' => '',
   'module' => '',
-  'state' => 'beta',
+  'state' => 'stable',
   'internal' => '',
   'uploadfolder' => 0,
   'createDirs' => '',
@@ -27,27 +27,27 @@ $EM_CONF[$_EXTKEY] = array (
   'clearCacheOnLoad' => 0,
   'lockType' => '',
   'author_company' => '',
-  'version' => '1.0.1',
-  'constraints' => 
+  'version' => '1.0.2',
+  'constraints' =>
   array (
-    'depends' => 
+    'depends' =>
     array (
       'php' => '5.3.2-7.1.99',
       'typo3' => '6.2.99-7.6.99',
     ),
-    'conflicts' => 
+    'conflicts' =>
     array (
     ),
-    'suggests' => 
+    'suggests' =>
     array (
     ),
   ),
   '_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"783d";s:28:"class.tx_robots_postproc.php";s:4:"c31a";s:12:"ext_icon.gif";s:4:"c800";s:17:"ext_localconf.php";s:4:"80a4";s:14:"ext_tables.php";s:4:"f3bf";s:14:"ext_tables.sql";s:4:"d047";s:16:"locallang_db.xml";s:4:"2103";s:14:"doc/manual.sxw";s:4:"e395";}',
-  'suggests' => 
+  'suggests' =>
   array (
   ),
-  'comment' => 'TYPO3 6.2 compatibility update',
-  'user' => 'dmitry',
+  'comment' => 'TYPO3 7.6 compatibility update',
+  'user' => 'cweiske',
 );
 
 ?>
